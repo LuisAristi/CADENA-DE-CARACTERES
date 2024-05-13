700cf8ac2b9a72570fbe84a3e3c5eea4d1a13bf3
LuisAristi <santiaristi2015@gmail.com>
2024-05-13 03:24:28 -0500

    cambiar color del texto OK

--- libreria.h
+++ libreria.h
@@ -11,8 +11,76 @@ void mostrarMenu() {
     cout << "6. Salir\n";
 }
 
-void cambiarColorTexto() {
-    // Implementa la función para cambiar el color del texto
+void cambiarColorTexto(char texto[]) {
+    // Códigos de escape ANSI para colores
+    char RESET[] = "\033[0m";
+    char BLACK[] = "\033[30m";
+    char RED[] = "\033[31m";
+    char GREEN[] = "\033[32m";
+    char YELLOW[] = "\033[33m";
+    char BLUE[] = "\033[34m";
+    char MAGENTA[] = "\033[35m";
+    char CYAN[] = "\033[36m";
+    char WHITE[] = "\033[37m";
+
+    // Mostrar menú de colores
+    cout << "=== CAMBIAR COLOR DEL TEXTO ===\n";
+    cout << "1. Negro\n";
+    cout << "2. Rojo\n";
+    cout << "3. Verde\n";
+    cout << "4. Amarillo\n";
+    cout << "5. Azul\n";
+    cout << "6. Magenta\n";
+    cout << "7. Cyan\n";
+    cout << "8. Blanco\n";
+    cout << "9. Reset (Volver al color predeterminado)\n";
+
+    int opcion;
+    cout << "Seleccione el color deseado: ";
+    cin >> opcion;
+
+    // Cambiar el color según la opción seleccionada
+    switch(opcion) {
+        case 1:
+            cout << BLACK;
+            break;
+        case 2:
+            cout << RED;
+            break;
+        case 3:
+            cout << GREEN;
+            break;
+        case 4:
+            cout << YELLOW;
+            break;
+        case 5:
+            cout << BLUE;
+            break;
+        case 6:
+            cout << MAGENTA;
+            break;
+        case 7:
+            cout << CYAN;
+            break;
+        case 8:
+            cout << WHITE;
+            break;
+        case 9:
+            cout << RESET;
+            break;
+        default:
+            cout << "Opción inválida. Seleccionando color predeterminado.\n";
+    }
+
+    // Imprimir el texto con el color seleccionado
+    int i=0;
+    while (texto[i] != '\0') {
+        cout << texto[i];
+        i++;
+    }
+
+    cout << RESET; // Restaurar el color predeterminado
+    cout << "\nEl color del texto ha sido cambiado.\n";
 }
 
 void cambiarMayusculaMinuscula() {
--- main.c++
+++ main.c++
@@ -2,6 +2,7 @@
 
 int main() {
     char opcion;
+    
 
     do {
         mostrarMenu();
